a48df174d4108d476be0545e36702d3a788f8bbb
Andrew <andrew.alan.sm@gmail.com>
2025-03-09 18:42:20 +0000

    complete reading time component

--- src/components/TextReadingTime.jsx
+++ src/components/TextReadingTime.jsx
@@ -1,7 +1,14 @@
 export default function TextReadingTime({ text }) {
+    // Roughly the average reading speed of an adult.
+    const READING_SPEED = 250; // words per minute
+
+    const wordCount = text.split(" ").length;
+    const readingTime = Math.ceil(wordCount / READING_SPEED);
+
     return (
         <section>
-            Approx. reading time: <span>{"< 1 minute"}</span>
+            Approx. reading time:{" "}
+            {readingTime === 1 ? "< 1 minute." : `< ${readingTime} minutes.`}
         </section>
     );
 }
