ae6945fc4ea8109cdc8eb99ab93de7318e362583
Andrew <andrew.alan.sm@gmail.com>
2025-03-09 19:03:13 +0000

    complete text density functionality

--- src/components/TextDensityCounter.jsx
+++ src/components/TextDensityCounter.jsx
@@ -1,10 +1,28 @@
 export default function TextDensityCounter({ text }) {
+    let letterCounts = {};
+
+    for (let letter of text.matchAll(/[a-z]/gi)) {
+        if (letter in letterCounts) {
+            letterCounts[letter]++;
+        } else {
+            letterCounts[letter] = 1;
+        }
+    }
+
+    // Sorted descending by count.
+    const sortedLetters = Object.entries(letterCounts).sort(
+        ([, a], [, b]) => b - a
+    );
+
     return (
         <section>
             <h3>Letter Density</h3>
             <ol>
-                <li>E: 1</li>
-                <li>D: 20</li>
+                {sortedLetters.map(([letter, count]) => (
+                    <li key={letter}>
+                        {letter}: {count}
+                    </li>
+                ))}
             </ol>
         </section>
     );
