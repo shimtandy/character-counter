169806a15757ac54ee77886a141157ca816f7f8d
Andrew <andrew.alan.sm@gmail.com>
2025-03-09 19:51:45 +0000

    add dark/light img toggles for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,30 @@ import sunIcon from "/images/icon-sun.svg";
 import moonIcon from "/images/icon-moon.svg";
 import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import "./styles/header.css";
 
 export default function Header({ setTheme }) {
     let theme = useContext(ThemeContext);
+
+    let buttonImgSrc = null;
+    let logoImgSrc = null;
+
+    if (theme === "light") {
+        buttonImgSrc = moonIcon;
+        logoImgSrc = lightLogo;
+    } else if (theme === "dark") {
+        buttonImgSrc = sunIcon;
+        logoImgSrc = darkLogo;
+    }
+
     return (
-        <header>
-            <img src={lightLogo} alt="Logo" />
+        <header className="pageHeader">
+            <img src={logoImgSrc} alt="Logo" />
 
             <button
                 onClick={() => setTheme(theme === "light" ? "dark" : "light")}
             >
-                <img src={moonIcon} alt="Light theme" />
+                <img src={buttonImgSrc} alt="Light theme" />
             </button>
         </header>
     );
